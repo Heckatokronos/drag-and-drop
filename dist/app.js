8e01f8349b815267219871ef1ab492730f4971cb
Heckatokronos <Heckatokronos@gmail.com>
2023-05-14 20:50:49 +0700

    Render project list

--- dist/app.js
+++ dist/app.js
@@ -113,5 +113,32 @@ class PJInput {
 __decorate([
     autobind
 ], PJInput.prototype, "submit", null);
+class PJList {
+    type;
+    templateElement;
+    hostElement;
+    element;
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById("project-list");
+        this.hostElement = document.getElementById("app");
+        const importedHtml = document.importNode(this.templateElement.content, true);
+        this.element = importedHtml.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector("ul").id = listId;
+        this.element.querySelector("h2").textContent =
+            this.type.toUpperCase() + " ПРОЕКТЫ";
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
+    }
+}
 const prjInput = new PJInput();
+const prjListsActive = new PJList("active");
+const prjListsFinished = new PJList("finished");
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -149,4 +149,40 @@ class PJInput {
   }
 }
 
+class PJList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    ) as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app") as HTMLDivElement;
+
+    const importedHtml = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedHtml.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " ПРОЕКТЫ";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
 const prjInput = new PJInput();
+const prjListsActive = new PJList("active");
+const prjListsFinished = new PJList("finished");
